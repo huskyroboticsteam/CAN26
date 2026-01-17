52f9fd8f9b1dd9686885daf94da56aa62281690c
Oliver Hines <olivermhines@gmail.com>
2026-01-17 12:25:33 -0800

    Add peripheral packets

---
+++ Packets/Peripheral.h
@@ -0,0 +1,43 @@
+#pragma once
+
+/**
+ * This file conists of helper functions for packet types from the Peripheral Domain
+ */
+
+#include "../CANPacket.h"
+#include "../CANPacketIDs.h"
+
+/**
+ * Constructs a packet to set the duty cycle of the device with the given pwm ID
+ * dutyCycle clamped to the range 0-100
+ */
+inline static CANPacket_t CANPeripheralPacket_SetPWMDutyCycle(CANDeviceUUID_t sender, CANDevice_t device, uint8_t peripheralID, float dutyCycle) {
+    CANPacket_t result = {
+        .device = device,
+        .contentsLength = 5,
+        .command = CAN_PACKET_ID__PWM_DUTY_CYCLE,
+        .senderUUID = sender,
+        .contents = {peripheralID}
+    };
+    if (dutyCycle < 0) {
+        dutyCycle = 0;
+    } else if (dutyCycle > 100) {
+        dutyCycle = 100;
+    }
+    CANStoreFloat32(result.contents + 1, dutyCycle);
+    return result;
+}
+
+/**
+ * Constructs a packet to set the color of the rover LED strips needed for competition
+ */
+inline static CANPacket_t CANPeripheralPacket_SetRoverLEDColor(CANDeviceUUID_t sender, CANDevice_t device, uint8_t red, uint8_t green, uint8_t blue) {
+    return (CANPacket_t){
+        .device = device,
+        .contentsLength = 3,
+        .command = CAN_PACKET_ID__ROVER_LED_COLOR,
+        .senderUUID = sender,
+        .contents = {red, green, blue}
+    };
+}
+
